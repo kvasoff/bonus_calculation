6a28c0529c56977300d4326f94088406e8afd19e
kvasoff <kvasov.one@gmail.com>
2022-04-26 15:31:53 +0300

    update

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int startigScore = 100; // Начальный счет
-        int refill = 1000; // Пополнение счета
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите сумму пополнения: ");
+        int refill = in.nextInt();
+
+        int startigScore = 0; // Начальный счет
         int finalScore = startigScore + refill; // Итоговый счет
         int bonus = refill / 100; // Количество бонусов
 
@@ -12,5 +18,6 @@ public class Main {
             System.out.println("Вам начислено: " + 0 + " бонусов");
             System.out.println("Ваш счет на текущий момент: " + (finalScore));
         }
+
     }
 }
