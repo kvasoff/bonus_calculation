3986368cf4d1cd119732a4d191b04d555b03b1a2
kvasoff <kvasov.one@gmail.com>
2022-04-26 15:18:57 +0300

    update

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         int startigScore = 100; // Начальный счет
-        int refill = 1100; // Пополнение счета
+        int refill = 1000; // Пополнение счета
         int finalScore = startigScore + refill; // Итоговый счет
-        int bonus = 0; // Количество бонусов
+        int bonus = refill / 100; // Количество бонусов
 
-        if (refill >= 1000) {
-            System.out.println("Вам начислено: " + (bonus = refill/100) + " бонусов");
+        if (refill > 1000) {
+            System.out.println("Вам начислено: " + bonus + " бонусов");
             System.out.println("Ваш счет на текущий момент: " + (finalScore + bonus));
         } else {
-            System.out.println("Вам начислено: " + bonus  + " бонусов");
+            System.out.println("Вам начислено: " + 0 + " бонусов");
             System.out.println("Ваш счет на текущий момент: " + (finalScore));
         }
     }
